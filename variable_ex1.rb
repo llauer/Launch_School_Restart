217f9f4de357fd44a14e9173e9582076a003de64
Larry Lauer <larry.lauer.3d@gmail.com>
2016-10-18 00:00:29 -0700

    Added variable exercises.

---
+++ variable_ex1.rb
@@ -0,0 +1,9 @@
+# asks for a name and prints a greeting #
+# modified to ask for first and last name. #
+puts "What is your first name?"
+firstname = gets.chomp
+puts "What is your last name?"
+lastname = gets.chomp
+
+
+puts 'Hello, ' + firstname + ' ' + lastname + ' nice to meet you.'
